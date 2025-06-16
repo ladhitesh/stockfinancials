69735fe64d2d11feffc135b2ade004292f9433b3
Hitesh Lad <ladhitesh@gmail.com>
2025-06-16 10:36:03 +0530

    Add files via upload

---
+++ Daily_Prices.py
@@ -0,0 +1,58 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 13:38:06 2023
+
+@author: tamme
+"""
+import yfinance as yf
+import pandas as pd
+from pandas.tseries.offsets import BDay
+
+# Load Tickers and dates from file
+tkrs=pd.read_excel(r'exlConfig.xlsx',sheet_name='Config',usecols='B:B')
+dates=pd.read_excel(r'exlConfig.xlsx',sheet_name='Config',nrows=1,usecols='F:H')
+tkrs=tkrs['Tickers'].values.tolist()
+#print(tkrs)
+overRide=dates['Override'].values.tolist()
+overRide=overRide[0]
+current_date = pd.Timestamp.now().normalize()
+# Determine the dates for which data has to be downloaded
+if overRide.upper()=='Y':
+    last_working_date = current_date - BDay(0)    
+    last_working_date=last_working_date.strftime('%Y-%m-%d')    
+    end_date = last_working_date
+    last_working_date = current_date - BDay(7)    
+    last_working_date=last_working_date.strftime('%Y-%m-%d')    
+    start_date = last_working_date
+else:    
+    dates
+    start_date = dates['StartDate'].tolist()
+    end_date = dates['EndDate'].tolist()
+    start_date = start_date[0].strftime('%Y-%m-%d')    
+    end_date = end_date[0].strftime('%Y-%m-%d')   
+
+i=0
+
+dataset=pd.DataFrame()
+yearly_avg_prices=pd.DataFrame()
+dataset = yf.download(tkrs, start=start_date, end=end_date, auto_adjust=False,multi_level_index=False)
+'''
+# Download historical data for each ticker
+for tkr in tkrs:
+    print(tkr,"\t",i)
+    data = yf.download(tkr, start=start_date, end=end_date, auto_adjust=False,multi_level_index=False)      
+    data.rename(columns={'Close':tkr},inplace=True)
+    #print(data[tkr])
+    #dataset = dataset.droplevel(level=0)
+    #print(dataset)     
+    if len(dataset)>0: dataset=pd.merge(dataset,data[tkr],left_index=True,right_index=True,how='outer')
+    else: dataset=data    
+    i+=1
+'''
+dailydata=dataset.copy()
+dataset=dailydata.copy()
+dataset.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+dataset.index=dataset.index.date
+print(dataset)
+dataset.T.to_excel(r'DailyPrices.xlsx',index='False')
